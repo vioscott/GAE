2e62bcc6500783f3f17fdcacf0e72e82b14926d1
vioscott <vioscott2020@gmail.com>
2024-07-11 03:21:55 +0100

    Configure database connection

--- config/db.js
+++ config/db.js
@@ -4,11 +4,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const db = new pg.Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+    // user: process.env.DB_USER,
+    // host: process.env.DB_HOST,
+    // database: process.env.DB_NAME,
+    // password: process.env.DB_PASSWORD,
+    // port: process.env.DB_PORT,
 });
 
 db.connect().catch(err => console.error('Error connecting to the database:', err));
